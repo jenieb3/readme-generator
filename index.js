bb292b1a15fb678be8e49c3529061e138f2e2f4b
jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-02-19 18:46:22 +0000

    moved javascript files out of starter folder

---
+++ index.js
@@ -0,0 +1,84 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a brief description of your project:'
+        },
+        {
+            type: 'input',
+            name: 'video',
+            message: 'Enter the URL of a video demonstrating your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use:'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide guidelines for how other developers can contribute to your project:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide examples of how to run tests:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project:',
+            choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        }
+    ]).then((data) => {
+        // Generate README file using data from user input
+        const readmeText = generateMarkdown(data);
+        const filePath = path.join(__dirname, 'README.md');
+        // function to write README file
+        fs.writeFile('README.md', readmeText, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('README file generated successfully!');
+            }
+
+        });
+
+
+    });
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
